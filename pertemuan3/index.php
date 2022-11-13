c7612eeaf850662343e6954d262e125075cec1f3
M. Wira Ade Kusuma <mail@mwiraadekusuma.my.id>
2022-11-13 09:15:15 +0700

    struktur kendali

--- pertemuan3/index.php
+++ pertemuan3/index.php
@@ -10,6 +10,7 @@
         echo "Hello World!".PHP_EOL;
         $a++;
     }
+
     echo "do while".PHP_EOL;
     $b=0;
     do {
@@ -25,18 +26,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .warna-baris{
+            background-color:silver;
+        }
+    </style>
 </head>
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
-        <?php
-            for ($i=1; $i <= 3; $i++) { 
-                echo "<tr>";
-                for ($j=0; $j <=5 ; $j++) { 
-                    echo "<td>$i,$j</td>";
-                }
-                echo "</tr>";
-            }
-        ?>
+//        <?php
+            // for ($i=1; $i <= 3; $i++) { 
+                // echo "<tr>";
+                // for ($j=0; $j <=5 ; $j++) { 
+                    // echo "<td>$i,$j</td>";
+                // }
+                // echo "</tr>";
+            // }
+        // ?>
+        <?php for ($i=1; $i<=5 ; $i++) : ?>
+            <?php if ($i % 2 == 1) : ?>
+            <tr class="warna-baris">
+                <?php else ?>
+                <tr></tr>
+            <?php endif;?>
+                <?php for ($j=1; $j<= 5 ; $j++) : ?> 
+                    <td><?= "$i, $j";?></td>
+                <?php endfor;?>
+            </tr>
+        <?php endfor; ?>
 
     </table>
 </body>
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,16 @@
+<?php
+// pengkondisian/cabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x=10;
+if ($x<20) {
+    echo "benar";
+}elseif ($x==20) {
+    echo "bingo";
+}else {
+    echo "salah";
+}
+?>
